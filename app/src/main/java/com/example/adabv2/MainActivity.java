dfa0408eb496611ee1778e00a42904c08f8135e3
Nissa Adila Hakim <nissaadila15@gmail.com>
2023-09-28 22:01:36 +0700

    cek

--- app/src/main/java/com/example/adabv2/MainActivity.java
+++ app/src/main/java/com/example/adabv2/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.adabv2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView ad;
+    Editable a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
