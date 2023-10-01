40b1703d04bfc537788f10b87ed8ff3ea34eefd6
priskilladrn <priskilla.adriani@gmail.com>
2023-10-02 01:25:55 +0700

    connect to db

---
+++ app/src/main/java/com/example/adabv2/SessionRequest.java
@@ -0,0 +1,18 @@
+package com.example.adabv2;
+
+import com.google.gson.annotations.SerializedName;
+
+public class SessionRequest {
+    @SerializedName("user_secret")
+    private String user_secret;
+    @SerializedName("date")
+    private String date;
+
+    public void setUser_secret(String user_secret) {
+        this.user_secret = user_secret;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
--- app/src/main/java/com/example/adabv2/Util/DateFormatter.java
+++ app/src/main/java/com/example/adabv2/Util/DateFormatter.java
@@ -9,18 +9,25 @@ import java.util.Date;
 
 public class DateFormatter {
     public static String DateToString(Date date) {
-        DateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy");
-        return  dateFormat.format(date);
+        return new SimpleDateFormat("yyyy-MM-dd").format(date);
     }
 
-    public static Date StringToDate(String date) {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy hh:mm a");
+    public static Date StringToDateMillisecond(String date) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         try {
             return formatter.parse(date);
         } catch (ParseException e) {
             Log.wtf("error", e);
             return new Date();
         }
+    }
+
+    public static String DateToTime(Date date) {
+        return new SimpleDateFormat("HH:mm a").format(date);
+    }
+
+    public static String DateToStringDate(Date date) {
+        return new SimpleDateFormat("dd MMM yyyy").format(date);
     }
 }
 
