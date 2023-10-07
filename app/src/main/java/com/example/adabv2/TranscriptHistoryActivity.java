ad83073b0d4018e114baac09f8d1c6ffbd89abf1
priskilladrn <priskilla.adriani@gmail.com>
2023-10-07 13:12:37 +0700

    ui transcript history

---
+++ app/src/main/java/com/example/adabv2/TranscriptHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.adabv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TranscriptHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transcript_history);
+    }
+}
