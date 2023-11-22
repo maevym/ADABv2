a894f2a78ecf9d22eed765030794fb1ae6e7f7f7
Nissa Adila Hakim <71497926+nissaadila@users.noreply.github.com>
2023-11-22 14:53:33 +0700

    update logic class session

--- app/src/main/java/com/example/adabv2/ClassSessionActivity.java
+++ app/src/main/java/com/example/adabv2/ClassSessionActivity.java
@@ -138,6 +138,13 @@ public class ClassSessionActivity extends AppCompatActivity implements ClassSess
 
     }
 
+    public ClassSessionRequest createClassSessionRequest(){
+        ClassSessionRequest classSessionRequest = new ClassSessionRequest();
+        classSessionRequest.setClass_id(classId);
+        Log.wtf("masuk class id", String.valueOf(classId));
+
+        return classSessionRequest;
+    }
 
 //    public void callFuncAPI (){
 //        classSessionList.clear();
